50364d6e528b3ddb2c7bab8448e8c163a4c4c15a
Praveena Kilaru <kilarupraveena03@gmail.com>
2026-08-17 12:25:33 +0530

    Allow production frontend CORS

--- backend/employee-management/src/main/java/com/praveena/employeemanagement/controller/EmployeeController.java
+++ backend/employee-management/src/main/java/com/praveena/employeemanagement/controller/EmployeeController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {
+        "http://localhost:5173",
+        "https://employee-management-frontend-i409.onrender.com"
+})
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
